bd0e2a27b1d7de8fe8d90bbcfcc7522f7730d909
weicap <66563616+weicap@users.noreply.github.com>
2022-08-27 13:33:22 -0500

    Add files via upload

---
+++ Practica 3/Codigo/quadtree.js
@@ -0,0 +1,28 @@
+class Point{
+	constructor(x,y,userData){
+		this.x=x;
+		this.y=y;
+		this.userData=userData;
+	}
+}
+
+class Rectangle{
+	constructor(x,y,w,h){
+		this.x=x;
+		this.y=y;
+		this.w=w;
+		this.h=h;
+	}
+// verifica si este objeto contiene un objeto Punto
+	points = new Point();
+	contains (point){
+
+	}
+
+// verifica si este objeto se intersecta con otro objeto Rectangle
+	intersects (range){
+
+	}
+}
+
+Rectangle.point()
