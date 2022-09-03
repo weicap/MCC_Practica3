7392238a2219ab210dbb316928239c45d85c474a
weicap <66563616+weicap@users.noreply.github.com>
2022-09-03 01:13:29 -0500

    Update sketch.js

--- QuadTree/sketch.js
+++ QuadTree/sketch.js
@@ -1,5 +1,6 @@
 let qtree;
-
+let cs =0;
+var cs2 ='';
 function setup() {
   createCanvas(400, 400);
   background(255);
@@ -8,7 +9,7 @@ function setup() {
 
   console.log(qtree);
   
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 3; i++) {
     let x = randomGaussian(width / 2, width / 8);
     let y = randomGaussian(height / 2, height / 8);
     let p = new Point(x, y);
@@ -31,8 +32,12 @@ function draw(){
     qtree.query(range,points);
 
     for(let p of points){
-        strokeWeight(4);
+        strokeWeight(12);
         point(p.x,p.y);
+        cs++;
+        cs2=cs2+'; ('+String(parseInt(p.x))+','+String(400-parseInt(p.y))+')';//,p.y);
+        //updateInfo();
+        //cs.removeAll(cs);
     }
 
     if (mouseIsPressed){
@@ -43,5 +48,22 @@ function draw(){
     }
     // background(0);
     // qtree.show();
+    updateInfo();
+    cs=0;
+    cs2=''
 }
 
+function updateInfo() {
+	let info = document.getElementsByClassName("Puntos")[0];
+    info.innerHTML = `
+    <tr>
+    <th>Número de Puntos:</th>
+    <th>${cs}<br></th>
+    </tr>
+
+    <tr>
+    <th>CS{}:</th>
+    <th>${cs2}<br></th>
+    </tr>
+    `;
+}
